3e59faab0fa058fd8087559df7f4e1430b7ad6e9
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-04-23 16:14:43 +0400

    4.23.2021

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -30,7 +30,7 @@ namespace WebAPI.Controllers
         {
             //Dependecy chain
 
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
 
             var result = _productService.GetAll();
             if (result.Success)
@@ -44,6 +44,17 @@ namespace WebAPI.Controllers
         public IActionResult GetById(int id)
         {
             var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
             if (result.Success)
             {
                 return Ok(result);
