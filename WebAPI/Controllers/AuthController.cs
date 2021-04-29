4f88f4ced7a9d5858a82c12bbef9fc53f5264523
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-04-29 11:59:32 +0400

    Code Refactoring

--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -1,4 +1,4 @@
-﻿using Business.Abstract;
+﻿    using Business.Abstract;
 using Entities.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -31,7 +31,7 @@ namespace WebAPI.Controllers
             var result = _authService.CreateAccessToken(userToLogin.Data);
             if (result.Success)
             {
-                return Ok(result.Data);
+                return Ok(result);
             }
 
             return BadRequest(result.Message);
